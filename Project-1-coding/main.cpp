ad62381131199ee6659ecee295fd6ec6a48be965
livewj <livewj@Lives-MacBook-Pro.local>
2016-09-06 19:27:02 +0200

    coding

--- Project-1-coding/main.cpp
+++ Project-1-coding/main.cpp
@@ -1,28 +1,110 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
+ofstream ofile;
+
 //This function uses Gaussian elimination to solve a tridiagonal matrix.
 //Our main problem is to solve the equation - u''(x) = f(x)
 
-double * vectors (unsigned int size,
-                          double *a, //lower diagonal is now a pointer
-                          double *b, //diagonal
-                          double *c, //upper diagonal
-                          double *b_tilde, //the solution, f(x)*h^2
-                          bool special = 0) //the function can use the general algorithm
-                                            //or the algorithm developed for our
-                                            // special case
-{
-    double *v = new double[size]; //the solution goes here
-    //General case
-    if (!special){
-        for (unsigned int i=1, i<si)
+//the given function f(x):
+double f(double x) {
+    return 100*exp(-10*x);
+}
+
+//this is the ananytical solution u(x):
+double analytical(double x) {
+    return 1 - (1 - exp(-10))*x - exp(-10*x);
+}
+
+
+//this function gives us a numerical solution to the differential-equation
+//by using Gaussian elimination.
+//this program takes two arguments from the command-line: the filename and the size, n
+int main(int argc, char* argv[]) {
+    char *outfilename; //output filename
+    int n;
+
+    //placing the command-line arguments
+    outfilename = argv[1];
+    n = atoi(argv[2]); //converting from ascii to int
+
+    //declaring constants
+    double h = 1./(n+1); //step size
+    double *x = new double[n+2];
+    double *b_tilde = new double[n+1];
+    b_tilde[0] = 0;
+    //vectors of the matrix A:
+    int *a = new int[n+1];
+    int *b = new int[n+1];
+    int *c = new int[n+1];
+    //the new elements on the diagonal
+    //double *diagonal_new = new double[n+1];
+    //solutions
+    double *u = new double[n+2]; //analytical
+    u[0] = 0;
+    double *v = new double[n+2]; //numerical
+    v[0] = 0;
+    double *e_tilde = new double[n+2];
+    e_tilde[0] = 0;
+    double *d_tilde = new double[n+2];
+    d_tilde[0] = 0;
+
+    //finding the values of x
+    for (int i=0; i<=n+1; i++) {
+        x[i] = i*h;
     }
-}
-                  
 
-int main()
-{
+    //finding the values of b_tilde and u.
+    //filling up the vectors a, b and c
+    for (int i=1; i<=n; i++) {
+        b_tilde[i] = h*h*f(x[i]);
+        u[i] = analytical(x[i]);
+        a[i] = -1;
+        b[i] = 2;
+        c[i] = -1;
+
+    }
+    a[1] = 0;
+    c[n] = 0;
+
+    //finding the numerical solution using Gauss elimination
+    //forward substitution:
+    //initial conditions
+    d_tilde[1] = b[1];
+
+    e_tilde[1] = b_tilde[1]/d_tilde;
+    for (int i=2; i<=n; i++) {
+        //diagonal_new[i] = c[i-1]/d_tilde;
+
+        d_tilde[i] = b[i] - (c[i-1]/d_tilde[i-1])*a[i];
+        e_tilde[i] =(b_tilde[i] - e_tilde[i-1]*a[i])/d_tilde[i];
+    }
+
+    //backward substitution:
+    for (int i=n-1; i>=1; i--) {
+        v[i] = e_tilde[i] - (c[i]/d_tilde[i])*v[i+1];
+
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
